228d81c59f59eb14c7012ad97c70e455ceaecc28
Patricio Castro <luckydrawings@gmail.com>
2020-12-24 12:26:16 +0100

    modal gallery added, dropdown menu fixed

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const pages = [
             { label: 'vestidos', icon: './assets/wedding-dress.png' },
             { label: 'bolsos', icon: './assets/wedding-dress.png' },
             { label: 'pantalones', icon: './assets/wedding-dress.png' },
-            { label: 'shorts', icon: './assets/wedding-dress.png' }
+            { label: 'shorts', icon: './assets/wedding-dress.png' },
         ]
     },
     {
@@ -64,7 +64,8 @@ const pages = [
         links: [
             { label: 'juguetes', icon: './assets/baby-face.png' },
             { label: 'disfraces', icon: './assets/baby-face.png' },
-            { label: 'ropa', icon: './assets/baby-face.png' }
+            { label: 'ropa', icon: './assets/baby-face.png' },
+            { label: 'bodies', icon: './assets/baby-face.png' }
         ]
     },
     {
@@ -78,11 +79,128 @@ const pages = [
     }
 ]
 
+const galleryImg = [
+    {
+        id: '1',
+        images: [
+            {
+                url: './assets/image 10.png', title: 'bañadores'
+            },
+            {
+                url: './assets/swim-1.jpg', title: 'bañadores-1'
+            },
+            {
+                url: './assets/swim-2.jpg', title: 'bañadores-2'
+            },
+            {
+                url: './assets/swim-3.jpg', title: 'bañadores-3'
+            },
+            {
+                url: './assets/swim-4.jpg', title: 'bañadores-4'
+            },
+            {
+                url: './assets/swim-5.jpg', title: 'bañadores-5'
+            },
+        ]
+    },
+    {
+        id: '2',
+        images: [
+            {
+                url: './assets/image 11.png', title: 'vestidos'
+            },
+            {
+                url: './assets/dress-1.jpg', title: 'vestidos-1'
+            },
+            {
+                url: './assets/dress-2.jpg', title: 'vestidos-2'
+            },
+            {
+                url: './assets/dress-3.jpg', title: 'vestidos-3'
+            },
+            {
+                url: './assets/dress-4.jpg', title: 'vestidos-4'
+            },
+            {
+                url: './assets/dress-5.jpg', title: 'vestidos-5'
+            },
+        ]
+    },
+    {
+        id: '3',
+        images: [
+            {
+                url: './assets/image 12.png', title: 'bolsos'
+            },
+            {
+                url: './assets/bags-1.jpg', title: 'bolsos-1'
+            },
+            {
+                url: './assets/bags-2.jpg', title: 'bolsos-2'
+            },
+            {
+                url: './assets/bags-3.jpg', title: 'bolsos-3'
+            },
+            {
+                url: './assets/bags-4.jpg', title: 'bolsos-4'
+            },
+            {
+                url: './assets/bags-5.jpg', title: 'bolsos-5'
+            },
+        ]
+    },
+    {
+        id: '4',
+        images: [
+            {
+                url: './assets/image 9.png', title: 'panatalones cortos'
+            },
+            {
+                url: './assets/shorts-1.jpg', title: 'panatalones cortos-1'
+            },
+            {
+                url: './assets/shorts-2.jpg', title: 'panatalones cortos-2'
+            },
+            {
+                url: './assets/shorts-3.jpg', title: 'panatalones cortos-3'
+            },
+            {
+                url: './assets/shorts-4.jpg', title: 'panatalones cortos-4'
+            },
+            {
+                url: './assets/shorts-5.jpg', title: 'panatalones cortos-5'
+            },
+        ]
+    },
+    {
+        id: '5',
+        images: [
+            {
+                url: './assets/image 13.png', title: 'shorts'
+            },
+            {
+                url: './assets/shorts-6.jpg', title: 'shorts-1'
+            },
+            {
+                url: './assets/shorts-7.jpg', title: 'shorts-2'
+            },
+            {
+                url: './assets/shorts-8.jpg', title: 'shorts-3'
+            },
+            {
+                url: './assets/shorts-9.jpg', title: 'shorts-4'
+            },
+            {
+                url: './assets/shorts-10.jpg', title: 'shorts-5'
+            },
+        ]
+    }
+]
+
 
 const linkBtns = [...document.querySelectorAll('.subpages')];
 
 const submenu = document.querySelector('.submenu');
-//const subitems = document.querySelector('.subitems');
 const nav = document.querySelector('.nav');
 const header = document.querySelector('.slider-wrapper')
 const products = document.querySelector('.products')
@@ -168,20 +286,11 @@ linkBtns.forEach((btn) => {
     btn.addEventListener('mouseover', function (e) {
 
         const text = e.currentTarget.textContent
-        const tempBtn = e.currentTarget.getBoundingClientRect()
-        console.log(tempBtn);
-        const center = (tempBtn.height + tempBtn.width);
-        const top = tempBtn.top + 160;
-
         const tempPage = pages.find((link) => link.page === text);
 
         if (tempPage) {
             const { page, links } = tempPage;
-
             submenu.classList.add('show');
-            submenu.style.left = `${center}px`;
-            submenu.style.top = `${top}px`;
-
 
             submenu.innerHTML = `
       <section>
@@ -189,17 +298,15 @@ linkBtns.forEach((btn) => {
       <div class="submenu-center">
       ${links
                     .map((link) => {
-                        return `<div class="links-container"><div class="icon"><img  src="${link.icon}" alt=""></div><a  href="" class="subpages" id="btn-pages"> ${link.label}</a></div>`;
+
+                        return `<div class="sub-container"><div class="icon"><img  src="${link.icon}" alt=""></div><a  href="" class="subpages" id="btn-pages"> ${link.label}</a></div>`;
                     })
                     .join('')}
       </div>
       </section>
       `;
         }
-
     })
-
-
 })
 
 
@@ -220,26 +327,22 @@ products.addEventListener('mouseover', function (e) {
 let navMenu = document.querySelector(".sidebar-wrapper");
 let topMenu = document.querySelector(".sidebar-header");
 let bottomMenu = document.querySelector(".sidebar-footer");
-/* let itemsNav = document.querySelector(".nav-menu a"); */
-/* let burgerLines = document.querySelector(".lines"); */
-/* const sidebar = document.querySelector('.sidebar-links'); */
 const toggleBtn = document.querySelector('.toggle-btn');
 const closeBtn = document.querySelector('.close-btn');
 
-toggleBtn.addEventListener("click", () => {
 
-
+function openSidebar() {
     navMenu.classList.add("show");
     topMenu.classList.add("show")
     bottomMenu.classList.add("show")
-
-});
-closeBtn.addEventListener('click', () => {
+}
+function closeSidebar() {
     navMenu.classList.remove("show");
     topMenu.classList.remove("show")
     bottomMenu.classList.remove("show")
-})
-
+}
+closeBtn.addEventListener('click', closeSidebar)
+toggleBtn.addEventListener("click", openSidebar)
 
 /********** accordion categorias */
 
@@ -247,8 +350,7 @@ const questions = document.querySelectorAll('.accordion-container')
 
 questions.forEach(function (question) {
     const btn = question.querySelector(".accordion-btn");
-    const sidebarTitle = question.querySelector(".accordion-btn h2")
-    // console.log(btn);
+
 
     btn.addEventListener("click", function () {
         console.log(question);
@@ -264,3 +366,87 @@ questions.forEach(function (question) {
 
     });
 });
+
+/************ modal gallery */
+const imgBtns = [...document.querySelectorAll('.img-products')]
+const modalImg = document.querySelector('.main-img')
+const modal = document.querySelector('.modal')
+const closeModal = document.querySelector('.close-modal')
+const modalImages = document.querySelector('.modal-images')
+const nextButton = document.querySelector('.next-button')
+const prevButton = document.querySelector('.prev-button')
+const imageName = document.querySelector('.image-name')
+
+imgBtns.forEach((btn) => {
+    btn.addEventListener('click', function (e) {
+        const id = e.target.dataset.id
+        let mainImg = e.target
+        const currentItem = galleryImg.find((item) => item.id === id)
+
+        if (currentItem) {
+
+            const { images } = currentItem
+            setMainImage(mainImg)
+            modalImages.innerHTML = images
+                .map(function (image, index) {
+
+
+                    const { url, title } = image
+
+
+                    return `<img src="${url
+                        }" title="${title}" id="${index}" class="${index === 0 ? 'modal-img selected' : 'modal-img'}" alt="${title}"/>`;
+                })
+                .join('');
+
+        }
+
+        modal.classList.add('open')
+        modalImages.addEventListener('click', chooseImage)
+        closeSidebar()
+
+
+    })
+})
+closeModal.addEventListener('click', () => {
+    modal.classList.remove('open')
+})
+nextButton.addEventListener('click', () => {
+    nextImage()
+
+})
+
+prevButton.addEventListener('click', () => {
+    prevImage()
+
+})
+
+
+function setMainImage(selectedImage) {
+    modalImg.src = selectedImage.src
+    imageName.textContent = selectedImage.title
+}
+function nextImage() {
+    const selected = modalImages.querySelector('.selected')
+    const next = selected.nextElementSibling || modalImages.firstElementChild
+    selected.classList.remove('selected')
+    next.classList.add('selected')
+    setMainImage(next)
+}
+function prevImage() {
+    const selected = modalImages.querySelector('.selected');
+    const prev =
+        selected.previousElementSibling || modalImages.lastElementChild;
+    selected.classList.remove('selected');
+    prev.classList.add('selected');
+    setMainImage(prev);
+}
+function chooseImage(e) {
+    if (e.target.classList.contains('modal-img')) {
+        const selected = modalImages.querySelector('.selected');
+        selected.classList.remove('selected');
+
+        setMainImage(e.target);
+        e.target.classList.add('selected');
+    }
+};
